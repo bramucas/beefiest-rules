fdf00d065ce8fbba615f1a80f97a91b424c7d616
Brais Muñiz <mc.brais@gmail.com>
2021-10-13 17:49:11 +0200

    Adds Explainer class code and tests (it's mocked)

--- tests_beefiest_rules/unit/conftest.py
+++ tests_beefiest_rules/unit/conftest.py
@@ -4,7 +4,8 @@ fixtures for the tests.
 
 import pytest
 from pathlib import Path
-from tests_beefest_rules import get_sandbox_dir, rec_rmdir
+from tests_beefiest_rules import get_sandbox_dir, rec_rmdir
+
 
 @pytest.fixture(scope='session', autouse=True)
 def sandbox_dir():
@@ -14,10 +15,10 @@ def sandbox_dir():
     # After tests
     rec_rmdir(sandbox_dir)
 
+
 @pytest.fixture(scope='session')
 def sandbox_file() -> Path:
-
-    def _sandbox_file(filename:str) -> Path:
+    def _sandbox_file(filename: str) -> Path:
         return get_sandbox_dir() / filename
-    
+
     return _sandbox_file
